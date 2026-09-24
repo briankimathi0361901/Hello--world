6fdc1abefd34f75732276a08b42c380340ad7195
briankimathi0361901 <briankimathi036@gmail.com>
2026-09-24 15:23:06 +0300

    Add files via upload
    units.c

---
+++ units.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    //display the units offered this semester 
+    printf("units offered this semester:\n");
+    printf("introduction to programming\n");
+    printf("data structures\n");
+    printf("computer networks\n");
+    printf("database systems\n");
+    printf("software engineering\n");
+    
+    }
